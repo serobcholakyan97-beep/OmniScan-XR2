95f1b1c8e44e76c5576236aef360eaee6d7b548e
serobcholakyan97-beep <serobcholakyan97@gmail.com>
2026-04-28 16:37:32 -0700

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,3 +1,17 @@
+# ==============================================================================
+
+# PROPRIETARY AND CONFIDENTIAL
+
+# OmniScan-XR System - Copyright (c) 2026 Serob Cholakyan
+
+# This code is protected under the OmniScan-XR Proprietary License.
+
+# Commercial use or unauthorized field mining operations are strictly prohibited.
+
+# ==============================================================================
+
+
+
 package com.omniscan.xr
 
 
